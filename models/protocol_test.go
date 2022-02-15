cfff9669f02428a48a58d7c847c96e1fa48c37d6
SaCavid <sadiqov.cavid@gmail.com>
2022-02-16 00:35:15 +0400

    CI CD test Add unit tests

---
+++ models/protocol_test.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"fmt"
+	"testing"
+)
+
+type TestProtocol struct {
+	Protocol
+	B bool
+}
+
+func TestProtocol_Validate(t *testing.T) {
+
+	samples := []TestProtocol{
+		{
+			Protocol: Protocol{
+				To:        "Worker001",
+				Command:   "Status", // wrong command
+				Error:     false,
+				ErrCode:   0,
+				Msg:       "",
+				AdminChan: nil,
+			},
+			B: true, // must be true if its wrong data
+		},
+		{
+			Protocol: Protocol{
+				To:        "Worker001",
+				Command:   "REBOOT", // wrong command
+				Error:     false,
+				ErrCode:   0,
+				Msg:       "",
+				AdminChan: nil,
+			},
+			B: false, // must be true if its wrong data
+		},
+	}
+
+	for _, v := range samples {
+		testName := fmt.Sprintf("%s,%s", v.To, v.Command)
+		t.Run(testName, func(t *testing.T) {
+			err := v.Validate()
+			if err != nil && !v.B {
+				t.Errorf("%s", err.Error())
+			}
+		})
+	}
+}
---
+++ service/receiver_test.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"fmt"
+	"testing"
+)
+
+type TestLogin struct {
+	Login string
+	B     bool
+}
+
+// s0m4k3y.Worker001|SHUTDOWN
+func TestValidateLogin(t *testing.T) {
+	samples := []TestLogin{
+		{
+			Login: "",
+			B:     true,
+		},
+		{
+			Login: "s0m4k3y.Worker001",
+			B:     false,
+		},
+	}
+
+	for k, v := range samples {
+		testName := fmt.Sprintf("%d,%s", k, v.Login)
+		t.Run(testName, func(t *testing.T) {
+			_, err := ValidateLogin([]byte(v.Login))
+			if err != nil && !v.B {
+				t.Errorf("%s", err.Error())
+			}
+		})
+	}
+
+}
